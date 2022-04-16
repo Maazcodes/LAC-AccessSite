c6d2216cb22742fcbb32428e315ddb4ca185002d
Pravin <pravin@archive.org>
2022-04-16 00:02:43 +0000

    Wt2825 advanced search fixes

--- lac_access_site/lac/static/search.js
+++ lac_access_site/lac/static/search.js
@@ -3,21 +3,25 @@ function getSearchParams() {
   return window.location.search; 
 }
 
+function setSearchParams(params) {
+  const parsedParams = new URLSearchParams(params);
+  let inputs = document.getElementsByClassName('form-control');
+
+  parsedParams.forEach((paramValue, paramKey) => {
+    const input = inputs[paramKey]; 
+    if(input && paramValue.length!=0){
+      input.value = paramValue;
+    }
+  });
+}
+
 function getSearchResults(params) {
   var path = (document.documentElement.lang  == "fr") ? "/fr/search" : "/search"
   var reqUrl = path + params;
-  console.log(reqUrl);
   var response = fetch(new Request(reqUrl,{mode: 'cors', headers: new Headers({"X-Requesting-Page": window.location}) }))
                  .then(response => {
                   return response.text()
                   .then(text => {
-                        //console.log(text);
-                        /*var parser = new DOMParser();
-                        var htmlDom = parser.parseFromString(text, 'text/html');
-                        var items = htmlDom.getElementsByClassName("search-results");
-                        console.log(items);
-                        populateResults(items);*/
-
                         var searchContainer = document.getElementById('results');
                         searchContainer.innerHTML = text;
                   });      
@@ -25,10 +29,27 @@ function getSearchResults(params) {
 
 }
 
+function validateAdvSearch() {
+  let startDate = document.getElementById("startField").value;
+  let endDate = document.getElementById("endField").value; 
+ 
+  if(!!startDate && !!endDate){
+    let startValue = new Date(startDate).valueOf;
+    let endValue = new Date(endDate).valueOf;
+
+    if(startValue >= endValue){
+      let errorMsg=document.getElementById("s-date-error");
+      errorMsg.textContent="Invalid start date | Date de début non valide";
+      return false;
+    }
+  }
+  return true;
+}
+
 var params = getSearchParams();
 
 //console.log(params);
 if (!!params.length){
   var results = getSearchResults(params);
+  setSearchParams(params);
 }
-
